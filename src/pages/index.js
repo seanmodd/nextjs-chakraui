acae1e8af9eff4e97680b0694b59b9f03bec97ef
seanmodd <sean@vestaridefair.com>
2021-07-22 18:45:27 -0700

    1 file modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,22 @@ import {
   Code,
   List,
   ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+  ListItem
+} from '@chakra-ui/react';
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
+import { Hero } from '../components/Hero';
+import { Container } from '../components/Container';
+import { Main } from '../components/Main';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { CTA } from '../components/CTA';
+import { Footer } from '../components/Footer';
 
 const Index = () => (
   <Container height="100vh">
     <Hero />
     <Main>
       <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
+        Example repository of of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
       </Text>
 
       <List spacing={3} my={0}>
@@ -49,6 +49,6 @@ const Index = () => (
     </Footer>
     <CTA />
   </Container>
-)
+);
 
-export default Index
+export default Index;
